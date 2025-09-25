88cf54b4af538ea8e42452d45224d6c09b14cf19
Mohammed Ali <mohammedali88870@gmail.com>
2025-09-25 11:12:59 +0530

    created express server

--- DevTinder-bkd/src/app.js
+++ DevTinder-bkd/src/app.js
@@ -4,6 +4,9 @@ const app = express();
 app.use("/test",(req,res)=>{
     res.send("Hello  Express!");
 })
+app.use("/hello",(req,res)=>{
+    res.send("Hello  Hello Hello Express!");
+})
 
 app.listen(7777,() => {
     console.log("Server is running on port 3000...");
