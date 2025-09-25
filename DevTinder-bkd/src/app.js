33bab3f524f0d3ff37b410079851b5723675e3c3
Mohammed Ali <mohammedali88870@gmail.com>
2025-09-25 20:01:29 +0530

    Exploring get,post,delete

--- DevTinder-bkd/src/app.js
+++ DevTinder-bkd/src/app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
 
-app.use("/test",(req,res)=>{
+app.use("/hello",(req,res)=>{
+    res.send("HAAAA HAAAA HAAAAA HAAAAA")
+})
+app.get("/test",(req,res)=>{
     res.send("Hello  Express!");
 })
-app.use("/hello",(req,res)=>{
-    res.send("Hello  Hello Hello Express!");
+app.post("/hello",(req,res)=>{
+    res.send("Data sucessfully saved in DB");
 })
 
 app.listen(7777,() => {
